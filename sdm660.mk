50171918d7cf199baf66ea9aa44429e9ea15c47c
Sã Śâjjãd <sqsa93103@gmail.com>
2024-06-11 13:13:32 +0600

    lavender: decommonize common tree
    Signed-off-by: Sã Śâjjãd <sqsa93103@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -60,6 +60,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+    
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl:32 \
