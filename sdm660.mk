47858e9204df9ec432632ab1c8c2f56660241db0
Sã Śâjjãd <sqsa93103@gmail.com>
2024-06-23 18:33:51 +0600

    fix-up
    Signed-off-by: Sã Śâjjãd <sqsa93103@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -281,10 +281,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
-
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
